cdcca8fe600625d46b64dd3b6c3f5b57b0f16087
Enzob <Enzo.bagnis@etu.univ-amu.Fr>
2025-09-19 13:36:29 +0200

    Add .htaccess for URL rewriting and create index.php for action-based redirection

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+if (isset($_GET['action']) && $_GET['action'] !== '') {
+    $action = $_GET['action'];
+    if ($action === 'index') {
+        header("Location: index.html");
+    } elseif ($action === 'inscription') {
+        header("Location: views/formulaire.php");
+    } else {
+        header("Location: index.html");
+    }
+}
+
