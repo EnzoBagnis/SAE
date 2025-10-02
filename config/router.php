5d2b502854b35a82de7699e10fe77cb9afca3278
MARCUS-William-2326073aa <146956211+MARCUS-William-2326073aa@users.noreply.github.com>
2025-10-02 21:32:04 +0200

    bien voila j'ai fait le routage avec controller

---
+++ config/router.php
@@ -0,0 +1,51 @@
+<?php
+class Router {
+    
+    public function route() {
+        $action = $_GET['action'] ?? 'accueil';
+        
+        switch($action) {
+            case 'index':
+            case 'accueil':
+                $this->loadController('accueilController', 'index');
+                break;
+                
+            case 'inscription':
+                $this->loadController('inscriptionController', 'showView');
+                break;
+
+	    case 'connexion':
+                $this->loadController('connexionController', 'showView');
+                break;
+
+	    case 'accueil2':
+                $this->loadController('accueil2Controller', 'showView');
+                break;
+
+	    case 'mailverif':
+                $this->loadController('mailVerifController', 'showView');
+                break;
+                
+            default:
+                $this->loadController('accueilController', 'index');
+                break;
+        }
+    }
+    
+    private function loadController($controllerName, $method) {
+        $controllerFile = $_SERVER['DOCUMENT_ROOT'] . '/SAE/controllers/' . $controllerName . '.php';
+        if (file_exists($controllerFile)) {
+            require_once $controllerFile;
+            
+            $controllerInstance = new $controllerName();
+            
+            if (method_exists($controllerInstance, $method)) {
+                $controllerInstance->$method();
+            } else {
+                die("Méthode $method introuvable dans $controllerName");
+            }
+        } else {
+            die("Controller $controllerName introuvable. Chemin testé: $controllerFile");
+        }
+    }
+}
