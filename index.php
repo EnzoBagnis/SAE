0e4934a0e6d815faa02a6495ef66ff2ab2cebf61
MARCUS-William-2326073aa <146956211+MARCUS-William-2326073aa@users.noreply.github.com>
2025-10-03 09:34:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$request = str_replace('/SAE/', '', $request);
+$request = str_replace('/SAE', '', $request);
+$request = trim($request, '/');
+
+if (!empty($request) && !isset($_GET['action'])) {
+    $_GET['action'] = $request;
+}
 require_once 'config/router.php';
 
 $router = new Router();
