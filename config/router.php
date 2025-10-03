bd4c1446a0618e7da5a5dd5907143bd1bc4e45ea
MARCUS-William-2326073aa <146956211+MARCUS-William-2326073aa@users.noreply.github.com>
2025-10-03 10:00:53 +0200

    reback router.php

--- config/router.php
+++ config/router.php
@@ -33,7 +33,7 @@ class Router {
     }
     
     private function loadController($controllerName, $method) {
-        $controllerFile = dirname(__DIR__) . '/controllers/' . $controllerName . '.php';
+        $controllerFile = $_SERVER['DOCUMENT_ROOT'] . 'controllers/' . $controllerName . '.php';
         if (file_exists($controllerFile)) {
             require_once $controllerFile;
             
