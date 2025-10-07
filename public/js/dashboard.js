5be2824b515c6dd32c490f3a626f9aec9914cbca
Enzo Bagnis <enzo.bagnis@etu.univ-amu.fr>
2025-10-07 22:12:18 +0200

    Add logout confirmation and improve sitemap modal functionality

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,15 +1,20 @@
+function confirmLogout() {
+    if (confirm('Êtes-vous sûr de vouloir vous déconnecter ?')) {
+        window.location.href = '../controllers/deconnexion.php';
+    }
+}
 
-    function openSiteMap() {
+function openSiteMap() {
     document.getElementById('sitemapModal').style.display = 'block';
 }
 
-    function closeSiteMap() {
+function closeSiteMap() {
     document.getElementById('sitemapModal').style.display = 'none';
 }
 
-    window.onclick = function(event) {
+window.onclick = function(event) {
     const modal = document.getElementById('sitemapModal');
     if (event.target == modal) {
-    modal.style.display = 'none';
-}
+        modal.style.display = 'none';
+    }
 }
--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-function confirmLogout() {
-    if (confirm("Vous voulez bien vous déconecter ?")) {
-        window.location.href='../../controllers/deconnexion.php';
-    }
-}
