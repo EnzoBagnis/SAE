c39984be05462fc9979f17c886d359ead2a2826c
Enzob <Enzo.bagnis@etu.univ-amu.Fr>
2025-10-09 08:10:06 +0200

    Remove password visibility toggle and related styles from connexion and formulaire pages

--- views/formulaire.php
+++ views/formulaire.php
@@ -8,25 +8,6 @@
     <meta name="description" content="Ceci est une meta description">
     <title>StudTraj - Inscription</title>
     <link rel="stylesheet" href="../public/css/style.css">
-    <style>
-        .password-container {
-            position: relative;
-        }
-        .toggle-password {
-            position: absolute;
-            right: 10px;
-            top: 50%;
-            transform: translateY(-50%);
-            background: none;
-            border: none;
-            cursor: pointer;
-            padding: 5px;
-            color: #666;
-        }
-        .toggle-password:hover {
-            color: #333;
-        }
-    </style>
 </head>
 <body>
 
@@ -71,16 +52,7 @@ if (isset($_GET['erreur'])) {
         <input type="email" id="mail" name="mail" placeholder="Valeur" required><br>
 
         <label for="mdp">Mot de passe</label>
-        <div class="password-container" style="position: relative;">
-            <input type="password" id="mdp" name="mdp" placeholder="Valeur" required style="width: 100%; padding-right: 40px;">
-            <button type="button" class="toggle-password" onclick="togglePassword('mdp', this)">
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                    <circle cx="12" cy="12" r="3"></circle>
-                </svg>
-            </button>
-        </div>
-        <br>
+        <input type="password" id="mdp" name="mdp" placeholder="Valeur" required><br>
 
         <button type="submit" class="btn-submit" name="ok">Inscription</button>
 
@@ -90,21 +62,5 @@ if (isset($_GET['erreur'])) {
 
     </form>
 </div>
-
-<script>
-function togglePassword(inputId, button) {
-    const input = document.getElementById(inputId);
-    const svg = button.querySelector('svg');
-
-    if (input.type === 'password') {
-        input.type = 'text';
-        svg.innerHTML = '<path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line>';
-    } else {
-        input.type = 'password';
-        svg.innerHTML = '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle>';
-    }
-}
-</script>
-
 </body>
 </html>
