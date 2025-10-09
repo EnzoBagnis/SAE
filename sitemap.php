2d2ebb0bc29a955be3f32924676c25955a263f0d
Enzob <Enzo.bagnis@etu.univ-amu.Fr>
2025-10-09 16:42:52 +0200

    Rename sitemap.xml to sitemap.php and update Content-Type handling

--- sitemap.php
+++ sitemap.php
@@ -1,5 +1,8 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<?php
+header('Content-Type: application/xml; charset=utf-8');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>https://studtraj.alwaysdata.net/</loc>
         <lastmod>2025-10-09</lastmod>
