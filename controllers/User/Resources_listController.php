ab1626f9b51fcc634b3f37b8270ccb4156dae7ec
MARCUS-William-2326073aa <146956211+MARCUS-William-2326073aa@users.noreply.github.com>
2025-10-23 18:03:54 +0200

    Update Resources_listController.php

--- controllers/User/Resources_listController.php
+++ controllers/User/Resources_listController.php
@@ -4,7 +4,7 @@ namespace Controllers\User;
 require_once __DIR__ . '/../BaseController.php';
 
 
-class Resource_listController extends \BaseController {
+class Resources_listController extends \BaseController{
 
     /**
      * Show dashboard page
