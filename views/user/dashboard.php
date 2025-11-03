292c5458d594c2c3f4bbb0fa3402da0dfda92682
super <hamza.koliai@etu.univ-amu>
2025-11-03 11:03:03 +0100

    There is no longer a problem with the additional import button in the footer, following a clever modification on our part.

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -150,15 +150,6 @@
         </div>
     </div>
 
-    <!-- Bouton d'import à ajouter dans le top-menu après les liens de navigation -->
-    <button onclick="openImportModal()" class="btn-import-trigger">
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-            <polyline points="17 8 12 3 7 8"></polyline>
-            <line x1="12" y1="3" x2="12" y2="15"></line>
-        </svg>
-        Importer
-    </button>
 
     <!-- Modal Plan du site -->
     <div id="sitemapModal" class="modal">
