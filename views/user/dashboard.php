1f0ba1836723c8fbde5b34a50e6f583bcbc14df2
MARCUS-William-2326073aa <william83w2@gmail.com>
2026-01-06 11:15:29 +0100

    change import button

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -43,21 +43,22 @@ $initials = strtoupper(substr($user_firstname, 0, 1) . substr($user_lastname, 0,
 
     <nav class="nav-menu">
         <a href="/index.php?action=resources_list" class="active">Ressources</a>
+        <div class="user-info">
+            <?php
+            // Récupérer l'ID de la ressource depuis l'URL si présent
+            $current_resource_id = isset($_GET['resource_id']) ? (int)$_GET['resource_id'] : 'null';
+            ?>
+            <button onclick="openImportModal(<?= $current_resource_id ?>)" class="btn-import-trigger">
+                <svg style="width: 20px; height: 15px;" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2">
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                    <polyline points="17 8 12 3 7 8"></polyline>
+                    <line x1="12" y1="3" x2="12" y2="15"></line>
+                </svg>
+                Importer
+            </button>
     </nav>
-    <div class="nav-menu">
-        <?php
-        // Récupérer l'ID de la ressource depuis l'URL si présent
-        $current_resource_id = isset($_GET['resource_id']) ? (int)$_GET['resource_id'] : 'null';
-        ?>
-        <button onclick="openImportModal(<?= $current_resource_id ?>)" class="btn-import-trigger">
-            <svg style="width: 20px; height: 15px;" viewBox="0 0 24 24" fill="none"
-                 stroke="currentColor" stroke-width="2">
-                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                <polyline points="17 8 12 3 7 8"></polyline>
-                <line x1="12" y1="3" x2="12" y2="15"></line>
-            </svg>
-            Importer
-        </button>
+
         <!-- Affichage Profil -->
         <div class="user-profile">
             <div class="user-avatar">
